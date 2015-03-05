c794a499ce813b4a880f433922ea8079ba745d80
Michael Hausenblas <michael.hausenblas@gmail.com>
2015-03-05 22:59:03 +0000

    updates Spark launch config to correct value

--- launch-elsa.sh
+++ launch-elsa.sh
@@ -1,5 +1,5 @@
 ~/spark-1.2.0/bin/spark-submit \
   --class spark.elsa.OnlineSA \
-  --master 127.0.0.1:5050 \
+  --master mesos://127.0.1.1:5050 \
   target/elsa-1.0-SNAPSHOT-jar-with-dependencies.jar \
   elsa.conf
