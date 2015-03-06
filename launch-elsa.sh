ffaf010850daf392d9fd3d0178001dc8c8e1545f
Michael Hausenblas <michael.hausenblas@gmail.com>
2015-03-06 20:43:05 +0000

    fixes launch script (again!)

--- launch-elsa.sh
+++ launch-elsa.sh
@@ -1,5 +1,5 @@
 /home/vagrant/spark-1.2.0/bin/spark-submit \
         --class spark.elsa.OnlineSA \
         --master mesos://127.0.1.1:5050 \
-        target/elsa-1.0-SNAPSHOT-jar-with-dependencies.jar \
-        elsa.conf
+        /home/vagrant/elsa/target/elsa-1.0-SNAPSHOT-jar-with-dependencies.jar \
+        /home/vagrant/elsa/elsa.conf
