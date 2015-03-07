07a2387fa4bc0d2ef3f804c99523dbc083f58716
Michael Hausenblas <michael.hausenblas@gmail.com>
2015-03-07 12:18:39 +0000

    makes auto-scale parameter configurable via config

--- autoscale.py
+++ autoscale.py
@@ -37,8 +37,6 @@ else:
   FORMAT = '%(asctime)-0s %(message)s'
   logging.basicConfig(level=logging.INFO, format=FORMAT, datefmt='%Y-%m-%dT%I:%M:%S')
 
-
-# TBD: read all below values from config file on start-up, overriding defaults:
 TRAFFIC_INCREASE_THRESHOLD = 6 # difference between previous and current traffic
 SCALE_FACTOR = 2  # part of threshold number of instances should be scaled
 
@@ -54,8 +52,10 @@ SCALE_FACTOR = 2  # part of threshold number of instances should be scaled
 # Helper
 #
 
-def get_stats_file(elsa_config):
+def get_config_params(elsa_config):
     stats_file_path = ''
+    traffic_increase_threshold = 6
+    scale_factor = 2
     if os.path.exists(elsa_config):
         logging.info('Using %s as config file' %(elsa_config))
         lines = tuple(open(elsa_config, 'r'))
@@ -65,12 +65,18 @@ def get_stats_file(elsa_config):
                 cfg_param = line.split('=')[0].rstrip() # extract config parameter
                 if cfg_param == 'stats-file':
                     stats_file_path = line.split('=')[1].strip().translate(None, '"')
+                if cfg_param == 'batch-window':
+                    batch_window = int(line.split('=')[1].strip())
+                if cfg_param == 'traffic-increase-threshold':
+                    traffic_increase_threshold = int(line.split('=')[1].strip())
+                if cfg_param == 'scale-factor':
+                    scale_factor = int(line.split('=')[1].strip())
     else:
         logging.info('No config file provided.')
     logging.debug('[%s]' %(stats_file_path))
-    return stats_file_path
+    return (stats_file_path, batch_window, traffic_increase_threshold, scale_factor)
 
-def launch_elsa(marathon, stats_file):
+def launch_elsa(marathon, stats_file, scale_window):
     logging.info('Start monitoring the inbound traffic on topics using %s' %(stats_file))
     # make sure the stats file is properly initialized:
     if not os.path.exists(stats_file):
@@ -92,7 +98,7 @@ def launch_elsa(marathon, stats_file):
         with open(stats_file, 'r') as elsa_file:
             topic_traffic = int(elsa_file.read())
             topic_traffic_diff = topic_traffic - previous_topic_traffic
-            print('Difference in traffic since last period: %d' %(topic_traffic_diff))
+            print('Difference in traffic in last period: %d' %(topic_traffic_diff))
             previous_topic_traffic = topic_traffic
             
             current_instance_num = c.get_app('elsa').instances
@@ -109,8 +115,7 @@ def launch_elsa(marathon, stats_file):
                 else:
                     c.scale_app('elsa', 1)
                     print('Resetting number of instances to 1')
-            
-        time.sleep(6) # TBD: read 'batch-window' from conf and make slightly higher, hard coded for now
+        time.sleep(scale_window)
 
 ################################################################################
 # Main script
@@ -118,9 +123,14 @@ def launch_elsa(marathon, stats_file):
 if __name__ == '__main__':
     try:
         marathon = sys.argv[1] # Marathon URL to use
-        stats_file = get_stats_file(sys.argv[2]) # ElSA config file to use
+        (stats_file_path, batch_window, traffic_increase_threshold, scale_factor) = get_config_params(sys.argv[2])
         print('Using %s to monitor topic traffic' %(stats_file))
-        launch_elsa(marathon, stats_file)
+        if traffic_increase_threshold:
+            TRAFFIC_INCREASE_THRESHOLD = traffic_increase_threshold
+        if scale_factor:
+            SCALE_FACTOR = scale_factor
+        print('Using traffic increase threshold of %d and scale factor %d' %(TRAFFIC_INCREASE_THRESHOLD, SCALE_FACTOR))
+        launch_elsa(marathon, stats_file_path, batch_window)
     except Exception, e:
         print(e)
         print(__doc__)
